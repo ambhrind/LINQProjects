c244f933e30ac903430de45456d40acd0269b741
ambhrind <ambhrin@gmail.com>
2020-04-04 22:22:03 -0500

    Added a class where you can enumerate only one the names field and not on any other fields

---
+++ LINQProjects/EnumerateSpecificFields.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQProjects
+{
+    public class EnumerateSpecificFields
+    {
+        private string firstName, middleName, lastName;
+        private int age;
+        private string gender;
+
+        public EnumerateSpecificFields(string fNAme, string mName, string lNAme, int age, string gender)
+        {
+            firstName = fNAme;
+            middleName = mName;
+            lastName = lNAme;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        /// <summary>
+        /// This only provides the capability to iterate the names and not the other fields
+        /// </summary>
+        public IEnumerable<string> Names
+        {
+            get
+            {
+                yield return firstName;
+                yield return middleName;
+                yield return lastName;
+            }
+        }
+    }
+}
--- LINQProjects/Program.cs
+++ LINQProjects/Program.cs
@@ -7,10 +7,16 @@ namespace LINQProjects
         static void Main(string[] args)
         {
             var p = new MyCustomCollection(2, 4, 5, 6);
+            var n = new EnumerateSpecificFields("sean", "kenny", "johnson", 32, "male");
 
             foreach (var i in p)
             {
                 Console.WriteLine(i);
+            }
+
+            foreach (string name in n.Names)
+            {
+                Console.WriteLine(name);
             }
         }
     }
