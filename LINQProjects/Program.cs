7c78ae1d8897dbe709062284fb7fbabcd1178afd
ambhrind <ambhrin@gmail.com>
2020-04-04 20:55:58 -0500

    Add project files.

---
+++ LINQProjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
